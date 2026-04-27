a9681d8016c5e7a94c423b294b481375965b60c7
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-04-27 19:04:25 +0530

    fs

--- SQL/copied.sql
+++ SQL/copied.sql
@@ -11,3 +11,10 @@ GROUP BY
     E1.employee_id, E1.name
 ORDER BY 
     employee_id
+
+SELECT
+    E1.employee_id,
+    E1.name,
+    COUNT(E2.employee_id) AS reports_count,
+    ROUND(AVG(E2.age)) AS average_age
+FROM
