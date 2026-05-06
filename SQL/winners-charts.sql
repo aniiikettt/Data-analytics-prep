f97621baf781fa494def3731ebacc8ded0c48819
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-06 12:34:31 +0530

    solved the error

--- SQL/winners-charts.sql
+++ SQL/winners-charts.sql
@@ -5,15 +5,4 @@ WITH best_scores AS (
         MAX(score) AS best_score
     FROM scoretable
     GROUP BY event_id, participant_name
-    ),
-ranked_scores AS (
-    SELECT 
-        event_id,
-        participant_name,
-        best_score,
-        DENSE_RANK() OVER (
-            PARTITION BY event_id
-            ORDER BY best_score DESC
-        ) AS rnk
-    FROM best_scores
-)
+ 
