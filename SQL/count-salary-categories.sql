41af805e25c0030be47584ab21e8f29530a650a3
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-09 13:40:31 +0530

    updated the code

--- SQL/count-salary-categories.sql
+++ SQL/count-salary-categories.sql
@@ -3,3 +3,9 @@ SELECT
     Count(income) AS accounts_count
 FROM Accounts
 WHERE income < 20000
+UNION
+SELECT
+    "Average Salary" AS category,
+    Count(income) AS accounts_count
+FROM Accounts
+WHERE income >= 20000 AND income <= 50000
