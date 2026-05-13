7145c82ca32c107d7df7898bc3eab29460248c07
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-13 11:42:46 +0530

    solved in an optimal way

--- SQL/movie-rating.sql
+++ SQL/movie-rating.sql
@@ -10,3 +10,8 @@ UNION ALL
 (SELECT title AS results
 FROM Movies
 INNER JOIN MovieRating USING(movie_id)
+WHERE MONTH(created_at) = '02' AND YEAR(created_at) = '2020'
+GROUP BY title
+ORDER BY AVG(rating) DESC, title
+LIMIT 1
+)
