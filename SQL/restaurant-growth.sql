0b9f94f84d8484707c7d79ee6f0bcaf1c6c319cc
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-14 15:49:45 +0530

    updated

---
+++ SQL/restaurant-growth.sql
@@ -0,0 +1,6 @@
+SELECT visited_on ,
+        (
+             SELECT SUM(amount)
+            FROM Customer
+            WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+            AND c.visited_on
