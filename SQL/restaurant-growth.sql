fc1cecdbc0812990c4ea5976fbe41b846759c695
Aniket Kumar <aniket.kumardeoghar2001@gmail.com>
2026-05-14 15:51:00 +0530

    solved leetcode

--- SQL/restaurant-growth.sql
+++ SQL/restaurant-growth.sql
@@ -6,3 +6,7 @@ SELECT visited_on ,
             AND c.visited_on
             ) AS amount ,
             ROUND((
+                SELECT SUM(amount)/7
+                FROM Customer
+                WHERE visited_on BETWEEN DATE_SUB(c.visited_on, INTERVAL 6 DAY)
+                AND c.visited_on
